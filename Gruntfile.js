789773f9bcfacfd4c418b30a5dbc955b89c9d95a
zygist87 <zygist87@gmail.com>
2019-02-28 21:31:55 +0200

    4save

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,19 +9,29 @@ module.exports = function(grunt) {
                 cssDir: 'web/assets/css',
                 force: true
             },
-  
             dist: {
                 options: {
                     environment: 'production',
                     noLineComments: true
                 }
             },
+            dev: {
+                options: {
+                    noLineComments: true
+                }
+            },
+            watch: {
+                options: {
+                    noLineComments: true,
+                    watch: true
+                }
+            }
         },
         assemble: {
           options: {
               layoutdir: 'src/templates/layouts',
               layout: ['default.hbs'],
-              partials: ['src/templates/partials/{,*/}*.*'],
+              partials: ['src/templates/partials/{,*/}*.*', 'src/sprites/svg/*'],
               helpers: ['partial'],
               flatten: true
           },
@@ -32,17 +42,60 @@ module.exports = function(grunt) {
               src: ['src/templates/pages/en/*.hbs'],
               dest: './web'
           },
+      },
+      watch: {
+          options: {
+          },
+          dev: {
+              files: ['src/assets/sass/**/*.scss', 'src/templates/**/*.hbs'],
+              tasks: ['compass:dev', 'assemble:en']
+          },
+          handlebars: {
+              files: ['src/templates/*/*.hbs', 'src/templates/layouts/*.hbs' ],
+              tasks: ['assemble:en']
+          }
+      },
+      svg_sprite: {
+        generate: {
+            cwd: 'web/assets/vendor/material-design-icons',
+            src: [
+                '../../../../web/assets/images/ic_menu_24px.svg',
+                '../../../../web/assets/images/ic_cropfree_24px.svg',
+                '../../../../web/assets/images/ic_morehoriz_24px.svg',
+                '../../../../web/assets/images/ic_addalert_24px.svg',
+                '../../../../web/assets/images/ic_accountcircle_24px.svg',
+            ],
+            dest: 'src/sprites',
+            options: {
+                shape: {
+                    id: {
+                        generator: function(filename) {
+                            var id = filename.match(/ic_(\w+)_\d+/);
+                            return id[1];
+                        }
+                    },
+                },
+                mode: {
+                    symbol: {
+                        dest: ''
+                    }
+                }
+            }
         }
+    },
     });
   
     [
       'grunt-contrib-compass',
+      'grunt-contrib-watch',
       'grunt-assemble',
+      'grunt-svg-sprite',
   ].forEach(grunt.loadNpmTasks);
   
     // Default task(s).
     grunt.registerTask('default', [
-        'compass:dist'
+        'compass:dist',
+        'assemble:en'
     ]);
   
   };
